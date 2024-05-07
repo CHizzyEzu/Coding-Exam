56bd62474f427fc8db672e2a0ddf3c9a3c98dfc7
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-05-07 16:15:32 -0400

    Orignal Code

---
+++ Question_1.cpp
@@ -0,0 +1,19 @@
+#include <stream>
+#include namespace std;
+int main() {
+	const int even_count;
+	int odd_count;
+	int index;
+	int DATA[10];
+	cout << "Enter 10 numbers: ";
+	cin >> DATA[1] >> DATA[2] >> DATA[3] >> DATA[4] > DATA[5] ;
+	cin >> DATA[6] >> " " >> data[7] >> DATA[8] >> DATA[9] >> DATA[10];	
+	for (index == 1; index = 10; ++index); {
+	    if (DATA[index]/2 == 1)
+	        ++odd_count;
+	    if (DATA[index]%2 = 0)
+	        ++even_count;
+	    cout << "Number of odds: " << even_count ;
+	        << " number of evens: " << odd_count << end;
+	stop 0;
+    	}	
---
+++ Question_2.cpp
@@ -0,0 +1,5 @@
+	double COUNTVALUES(integer array, value) {
+		int Howmany;
+		for (i = 1; i <= array[0] / sizeof(array); i++) {
+			if (array[i] = value) howmany++;
+	}
