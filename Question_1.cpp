e938a2549f6d461ebc636d90ecac37d9843e8b25
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-05-07 20:28:12 -0400

    Revised Code

--- Question_1.cpp
+++ Question_1.cpp
@@ -1,19 +1,71 @@
-#include <stream>
-#include namespace std;
-int main() {
-	const int even_count;
-	int odd_count;
+// Revised question 1 code
+
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <string>
+using namespace std;
+
+// Global array and variable
+const int ARRAY_SIZE = 10;
+int DATA[10];
+
+int main() 
+{
+
+// Initialized variables for-for loops
+	int even_count = 0;
+	int odd_count = 0;
 	int index;
-	int DATA[10];
-	cout << "Enter 10 numbers: ";
-	cin >> DATA[1] >> DATA[2] >> DATA[3] >> DATA[4] > DATA[5] ;
-	cin >> DATA[6] >> " " >> data[7] >> DATA[8] >> DATA[9] >> DATA[10];	
-	for (index == 1; index = 10; ++index); {
-	    if (DATA[index]/2 == 1)
+
+// Start of the .txt file
+    std::ofstream outputFile;
+    outputFile.open("Junior.txt");
+
+    if (!outputFile.is_open()) // File opening parameter
+    {
+        cout << "Error: Unable to open file!" << endl;
+        return 1;
+    }
+
+// For loop prompts user to input data 10 times
+	cout << "Enter 10 numbers: " << endl;
+	for (index = 0; index < ARRAY_SIZE; ++index)	
+    {
+// Input validation (Integers only)
+        while(!(cin >> DATA[index])) 
+        {
+            cout << "Invalid input. Please enter a number: " << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n' );
+        }
+
+        cin >> DATA[index];
+    }
+
+// For loop to determine if the user data is even or odd
+	for (index = 0; index < ARRAY_SIZE; ++index)
+    {
+	    if (DATA[index]% 2 == 1)
+        {
 	        ++odd_count;
-	    if (DATA[index]%2 = 0)
-	        ++even_count;
-	    cout << "Number of odds: " << even_count ;
-	        << " number of evens: " << odd_count << end;
-	stop 0;
-    	}	
+        }
+	    else 
+        {
+            ++even_count;
+        }
+    }	
+
+// Printed output to terminal
+    cout << "Number of odds: " << even_count << endl;
+	cout << "Number of evens: " << odd_count << endl;
+
+//Printed output to file
+    outputFile << "Number of odds: " << even_count << endl;
+    outputFile << "Number of evens: " << odd_count << endl;
+
+    outputFile.close(); // File closes
+
+    return 0; // End of program
+}
+
